ea1b8a418b5660dadf2a4ebb6d468aa0685a0833
Benito Patino <contact@benitopatino.com>
2022-09-04 16:28:21 -0700

    add CreateTicketTest

---
+++ TrackItNow.Data.Test/TicketRepositoryTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackItNow.Models;
+using Xunit;
+
+namespace TrackItNow.Data.Test
+{
+    public class TicketRepositoryTest
+    {
+        [Theory]
+        [InlineData("Ticket Number 3", 1, 4, 3, "This is ticket number 3.", 2, "503E2D5E-F9E2-4421-8B7D-9297F3AA8EE1", "C2D0195B-C5A5-4A24-8D1A-CC2780F6F797", "2022-09-11")]
+        [InlineData("Ticket Number 4", 2, 1, 1, "This is ticket number 4.", 1, "77B7408D-5A86-4925-B993-C86B97C8F1B0", "BDADBFC4-6487-4BAC-B0D8-BAEB2CCDDD6C", "2022-03-21")]
+        [InlineData("Ticket Number 5", 3, 3, 2, "This is ticket number 5.", 3, "503E2D5E-F9E2-4421-8B7D-9297F3AA8EE1", "C2D0195B-C5A5-4A24-8D1A-CC2780F6F797", "2022-06-21")]
+        [InlineData("Ticket Number 6", 1, 2, 1, "This is ticket number 6.", 3, "77B7408D-5A86-4925-B993-C86B97C8F1B0", "BDADBFC4-6487-4BAC-B0D8-BAEB2CCDDD6C", "2022-12-21")]
+        public void CreateTicketTest(string Title, byte TicketResolutionId, byte TicketStatusId, byte TicketTypeId, string Description, byte PriorityId, Guid EmployeeId, Guid ProjectIds, string DueDate)
+        {
+            TicketRepository ticketRepository = new TicketRepository();
+
+            NewTicket newTicket = new NewTicket()
+            {
+                Title = Title,
+                TicketResolutionId = TicketResolutionId,
+                TicketStatusId = TicketStatusId,
+                TicketTypeId = TicketTypeId,
+                Description = Description,
+                PriorityId = PriorityId,
+                EmployeeId = EmployeeId,
+                ProjectId = ProjectIds,
+                DateDue = DateTime.Parse(DueDate)
+            };
+
+            var ticket = ticketRepository.Create(newTicket);
+
+            Assert.IsType<Ticket>(ticket);
+
+            Assert.Equal(newTicket.Title, ticket.Title);
+            Assert.Equal(newTicket.TicketResolutionId, ticket.TicketResolutionId);
+            Assert.Equal(newTicket.TicketStatusId, ticket.TicketStatusId);
+            Assert.Equal(newTicket.TicketTypeId, ticket.TicketTypeId);
+            Assert.Equal(newTicket.Description, ticket.Description);
+            Assert.Equal(newTicket.PriorityId, ticket.PriorityId);
+            Assert.Equal(newTicket.EmployeeId, ticket.EmployeeId);
+            Assert.Equal(newTicket.ProjectId, ticket.ProjectId);
+        }
+    }
+}
