f06df22b223506f1868cac744f26775f03f6b11e
Benito Patino <contact@benitopatino.com>
2022-09-05 15:58:37 -0700

    Add Create to ProjectRepository

---
+++ TrackItNow.Data/IProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackItNow.Models;
+
+namespace TrackItNow.Data
+{
+    public interface IProjectRepository
+    {
+        Project Create(Project newProject);
+    }
+}
---
+++ TrackItNow.Data/ProjectRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackItNow.Models;
+
+namespace TrackItNow.Data
+{
+    public class ProjectRepository
+    {
+        public Project Create(Project newProject)
+        {
+            string sql = @"
+                Insert into Project (Name, ProjectStatusId) Values (@pName, @pProjectStatusId)
+            ";
+
+            SqlConnection con = new SqlConnection(DbSettings.ConnectionString);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            var project = new Project()
+            {
+                Name = newProject.Name,
+                ProjectStatusId = newProject.ProjectStatusId
+            };
+
+            cmd.Parameters.Add("@pName", SqlDbType.VarChar).Value = project.Name;
+            cmd.Parameters.Add("@pProjectStatusId", SqlDbType.TinyInt).Value = project.ProjectStatusId;
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            return project;
+
+        }
+    }
+}
