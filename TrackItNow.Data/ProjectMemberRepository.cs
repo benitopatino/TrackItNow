0fe7151d8bd13455749e5a8941cf61d19aa3394d
Benito Patino <contact@benitopatino.com>
2022-09-05 17:21:32 -0700

    add and test Create in ProjectMemberRepository

--- TrackItNow.Data/ProjectMemberRepository.cs
+++ TrackItNow.Data/ProjectMemberRepository.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackItNow.Models;
 
 namespace TrackItNow.Data
 {
     public class ProjectMemberRepository : IProjectMemberRepository
     {
+        public ProjectMember Create(ProjectMember newProjectMember)
+        {
+            string sql = @"
+                Insert into ProjectMember (MemberId, ProjectId) Values (@pMemberId, @pProjectId)
+            ";
+
+            SqlConnection con = new SqlConnection(DbSettings.ConnectionString);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.Add("@pMemberId", SqlDbType.UniqueIdentifier).Value = Guid.Parse(newProjectMember.MemberId);
+            cmd.Parameters.Add("@pProjectId", SqlDbType.UniqueIdentifier).Value = Guid.Parse(newProjectMember.ProjectId);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            return newProjectMember;
+        }
     }
 }
